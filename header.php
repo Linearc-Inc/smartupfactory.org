88b816672fb5f19b5b8c7055d6237306b0b4b560
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-26 10:34:23 +0300

    Added navigation menu

--- header.php
+++ header.php
@@ -36,19 +36,21 @@
                 <div class="row">
                     <div class="col-12 d-flex flex-wrap justify-content-between align-items-center">
                         <div class="site-branding d-flex align-items-center">
-                        <a class="d-block" href="/" rel="home"><img class="d-block" src="https://smartupfactory.org/wp-content/uploads/2020/06/smart-up-light-blue.png" alt="logo"></a>
+                        <?php 
+                            if ( function_exists( 'the_custom_logo' ) ) {
+                                the_custom_logo();
+                            }
+                            ?>
                         </div><!-- .site-branding -->
-
                         <nav class="site-navigation d-flex justify-content-end align-items-center">
-                            <ul class="d-flex flex-column flex-lg-row justify-content-lg-end align-content-center">
-                            <li><a href="about-us">About us</a></li>
-                            <li><a href="cause">Causes</a></li>
-                            <li><a href="gallery">Gallery</a></li>
-                            <li><a href="whats-new">News</a></li>
-                            <li><a href="contact-us">Contact</a></li>
-                            <li><a href="team">Team</a></li>
-                            </ul>
-                        </nav><!-- .site-navigation -->
+                        <?php 
+                            wp_nav_menu(array(
+                            'theme_location'=>'main_menu',
+                            'container'       => false,
+                            'menu_class'=>'d-flex flex-column flex-lg-row justify-content-lg-end align-content-center',
+                            ))
+                            ?>
+                         </nav>
 
                         <div class="hamburger-menu d-lg-none">
                             <span></span>
