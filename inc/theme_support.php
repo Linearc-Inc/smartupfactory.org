cb9eb95071510430e6ec36afeeb6f95f34eafc70
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-26 10:09:49 +0300

    Added logo surport

--- inc/theme_support.php
+++ inc/theme_support.php
@@ -19,3 +19,16 @@ add_action('init', 'l_register_nav_menus');
 $formats = array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat');
 add_theme_support('post-formats', $formats);
 add_theme_support('post-thumbnails');
+
+function theme_name_custom_logo_setup()
+{
+    $defaults = array(
+    'height' => 10,
+    'width' => 20,
+    'flex-height' => true,
+    'flex-width' => true,
+    'header-text' => array('site-title', 'site-description'),
+    );
+    add_theme_support('custom-logo', $defaults);
+}
+   add_action('after_setup_theme', 'theme_name_custom_logo_setup');
