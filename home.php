e2eac018d59e3735b14ea70b65dec137f4773d3b
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-26 10:37:46 +0300

    Added innovation of the year

--- home.php
+++ home.php
@@ -40,3 +40,40 @@
                             </div><!-- .event-content-wrap -->
                         </div><!-- .event-wrap -->
 <?php endwhile; wp_reset_query(); ?>
+
+<?php $annual_innovation = get_post(get_theme_mod('innovation_of_the_year', '2')); ?>
+                <div class="col-12 col-lg-6">
+                    <div class="featured-cause">
+                        <div class="section-heading">
+                            <h2 class="entry-title">Innovation Of The Year</h2>
+                        </div><!-- .section-heading -->
+
+                        <div class="cause-wrap d-flex flex-wrap justify-content-between">
+                            <figure class="m-0">
+                                <?php echo get_the_post_thumbnail( $annual_innovation->ID); ?>
+                            </figure>
+
+                            <div class="cause-content-wrap">
+                                <header class="entry-header d-flex flex-wrap align-items-center">
+                                    <h3 class="entry-title w-100 m-0"><a href="<?php echo get_permalink($annual_innovation->ID); ?>"><?php echo $annual_innovation->post_title; ?></a></h3>
+                                    <div class="cats-links">
+                                        <a href="<?php echo get_permalink($annual_innovation->ID); ?>"><?php echo get_the_author_meta('display_name', $annual_innovation->post_author); ?></a>
+                                    </div><!-- .cats-links -->
+                                </header><!-- .entry-header -->
+
+                                <div class="entry-content">
+                                    <p class="m-0"><?php echo $annual_innovation->post_excerpt; ?></p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer mt-5">
+                                    <a href="<?php echo get_permalink($annual_innovation->ID); ?>" class="btn gradient-bg mr-2">Read More</a>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .cause-content-wrap -->
+
+                        </div><!-- .cause-wrap -->
+                    </div><!-- .featured-cause -->
+                </div><!-- .col -->
+            </div><!-- .row -->
+        </div><!-- .container -->
+    </div><!-- .home-page-events -->
+
