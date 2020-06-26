2d506a800b9801170759a7f05064b8bb02095741
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-26 10:18:14 +0300

    Added feature stories from the admin

--- causes.php
+++ causes.php
@@ -4,12 +4,27 @@
   * Template Name: causes.
   */
  ?>
-  <?php get_header(); ?>
+  <?php get_header();
+    $sticky_stories = new WP_Query(
+        array(
+            'post_type' => 'stories',
+            'posts_per_page' => 2,
+            'post__in' => get_option('sticky_posts'),
+            'meta_query' => array(
+                array(
+                    'key' => 'pseudosticky',
+                    'value' => 'on',
+                    'compare' => '=',
+                ),
+                //more meta conditions can be added here as arrays
+        ),
+        )
+    );
     <div class="page-header">
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1>Our Causes</h1>
+                    <h1>Youth Stories</h1>
                 </div><!-- .col -->
             </div><!-- .row -->
         </div><!-- .container -->
@@ -20,111 +35,45 @@
             <div class="row">
                 <div class="col-12">
                     <div class="section-heading">
-                        <h2 class="entry-title">Featured Cause</h2>
+                        <h2 class="entry-title">Featured Stories</h2>
                     </div><!-- .section-heading -->
                 </div><!-- .col -->
             </div><!-- .row -->
 
             <div class="row">
+
+<?php while ($sticky_stories->have_posts()) : $sticky_stories->the_post(); ?>
                 <div class="col-12 col-lg-6">
                     <div class="cause-wrap d-flex flex-wrap justify-content-between">
                         <figure class="m-0">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/featured-causes.jpg" alt="">
+                            <?php the_post_thumbnail('medium'); ?>
                         </figure>
 
                         <div class="cause-content-wrap">
                             <header class="entry-header d-flex flex-wrap align-items-center">
-                                <h3 class="entry-title w-100 m-0"><a href="#">Fundraiser for Kids</a></h3>
+                                <h3 class="entry-title w-100 m-0"><a href="#"><?php the_title(); ?></a></h3>
 
                                 <div class="posted-date">
-                                    <a href="#">Aug 25, 2018 </a>
+                                    <a href="#"><?php the_date(); ?> </a>
                                 </div><!-- .posted-date -->
 
                                 <div class="cats-links">
-                                    <a href="#">Ball Room New York</a>
+                                    <a href="#"><?php echo get_the_author_meta('display_name', the_author()); ?></a>
                                 </div><!-- .cats-links -->
                             </header><!-- .entry-header -->
 
                             <div class="entry-content">
-                                <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris. Lorem ipsum dolor sit amet, consectetur.</p>
+                                <p class="m-0"><?php the_excerpt(); ?></p>
                             </div><!-- .entry-content -->
 
                             <div class="entry-footer mt-5">
-                                <a href="#" class="btn gradient-bg mr-2">Donate Now</a>
+                                <a href="#" class="btn gradient-bg mr-2">Read Story</a>
                             </div><!-- .entry-footer -->
                         </div><!-- .cause-content-wrap -->
-
-                        <div class="fund-raised w-100">
-                            <div class="featured-fund-raised-bar barfiller">
-                                <div class="tipWrap">
-                                    <span class="tip"></span>
-                                </div><!-- .tipWrap -->
-
-                                <span class="fill" data-percentage="83"></span>
-                            </div><!-- .fund-raised-bar -->
-
-                            <div class="fund-raised-details d-flex flex-wrap justify-content-between align-items-center">
-                                <div class="fund-raised-total mt-4">
-                                    Raised: $56 880
-                                </div><!-- .fund-raised-total -->
-
-                                <div class="fund-raised-goal mt-4">
-                                    Goal: $70 000
-                                </div><!-- .fund-raised-goal -->
-                            </div><!-- .fund-raised-details -->
-                        </div><!-- .fund-raised -->
                     </div><!-- .cause-wrap -->
                 </div><!-- .col -->
-
-                <div class="col-12 col-lg-6">
-                    <div class="cause-wrap d-flex flex-wrap justify-content-between">
-                        <figure class="m-0">
-                            <img src="<?php echo get_template_directory_uri(); ?>/images/eye-for.jpg" alt="">
-                        </figure>
-
-                        <div class="cause-content-wrap">
-                            <header class="entry-header d-flex flex-wrap align-items-center">
-                                <h3 class="entry-title w-100 m-0"><a href="#">Fundraiser for Kids</a></h3>
-
-                                <div class="posted-date">
-                                    <a href="#">Aug 25, 2018 </a>
-                                </div><!-- .posted-date -->
-
-                                <div class="cats-links">
-                                    <a href="#">Ball Room New York</a>
-                                </div><!-- .cats-links -->
-                            </header><!-- .entry-header -->
-
-                            <div class="entry-content">
-                                <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tempus vestib ulum mauris. Lorem ipsum dolor sit amet, consectetur.</p>
-                            </div><!-- .entry-content -->
-
-                            <div class="entry-footer mt-5">
-                                <a href="#" class="btn gradient-bg mr-2">Donate Now</a>
-                            </div><!-- .entry-footer -->
-                        </div><!-- .cause-content-wrap -->
-
-                        <div class="fund-raised w-100">
-                            <div class="featured-fund-raised-bar barfiller">
-                                <div class="tipWrap">
-                                    <span class="tip"></span>
-                                </div><!-- .tipWrap -->
-
-                                <span class="fill" data-percentage="83"></span>
-                            </div><!-- .fund-raised-bar -->
-
-                            <div class="fund-raised-details d-flex flex-wrap justify-content-between align-items-center">
-                                <div class="fund-raised-total mt-4">
-                                    Raised: $56 880
-                                </div><!-- .fund-raised-total -->
-
-                                <div class="fund-raised-goal mt-4">
-                                    Goal: $70 000
-                                </div><!-- .fund-raised-goal -->
-                            </div><!-- .fund-raised-details -->
-                        </div><!-- .fund-raised -->
-                    </div><!-- .cause-wrap -->
-                </div><!-- .col -->
+<?php endwhile; wp_reset_query(); ?>
+            
             </div><!-- .row -->
         </div><!-- .container -->
     </div><!-- .featured-cause -->
