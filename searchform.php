7450321f82316f620a6e7709f3391a0602760432
Afasha Isakiye <afashaisakiye@gmail.com>
2020-06-27 01:31:24 +0300

    Added seaerch form

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<div class="search-widget">
+    <form class="flex flex-wrap align-items-center">
+        <input type="search" placeholder="Search...">
+        <button type="submit" class="flex justify-content-center align-items-center">GO</button>
+    </form><!-- .flex -->
+</div><!-- .search-widget -->
